b78b8232a8a3d37d0dbce0dff38ef68b3a5436b5
Olli Rousu <olli.rousu@hotmail.com>
2023-03-30 14:55:21 +0300

    training loop and some metrics + TODO

--- README.md
+++ README.md
@@ -9,3 +9,29 @@ Expects a data directory at root level containing annotations/ and images/
 Navigate to src/ directory and run main.py
 
 Otherwise relative paths get messed up and I can't be bothered to make it better
+
+## TODO
+
+* f1-metriikoita
+
+https://learnopencv.com/multi-label-image-classification-with-pytorch-image-tagging/
+ja kurssin slackissa on kans jotain
+
+Vois ainaki laskee että meiän f1 on parempi ku dummy :D
+
+* validaatio järkeväks
+
+epochit ylös et se earlystopper tekee jotain.
+
+printtaa jotain et saadaan joku estimate miten hyvä meiän malli on? 
+
+* hyperparametrei ja mallia yms voi parantaa jos jaksaa
+
+Lopuks?
+
+* Tallentaa parhaan mallin että voi sit helposti ajaa testi datan siitä läpi
+
+Lopullinen malli???
+* Menee vaan tolla mikä tulee ku laitetaan nykyisellä setillä läpi isoilla epocheilla + stopperilla
+* Voi laittaa sitä validaatio splitin määrää pienemmäks että enemmän treenidataa?
+* Ajaa semi isol epochi määrällä ja koko datasetillä. ei validaatiota täs vaihees. epochi määrä vaikka se mihin stoppasi ku oli train-validation setit
--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 import torch
-import numpy
+import numpy as np
 import data_loader
 from torchvision import models
 import torch.nn as nn
 
 #hyperparameters
 BATCH_SIZE = 5
-N_EPOCHS = 50
+N_EPOCHS = 2
 LR = 0.05
 NUM_CLASSES = 14
 
@@ -15,9 +15,7 @@ image_loader_train, image_loader_validation = data_loader.load_data(BATCH_SIZE)
 class MultilabelClassifier(nn.Module):
     def __init__(self):
         super().__init__()
-        self.resnet = models.resnet34(pretrained=True)
-        # MUUTA NIIN, ETTÄ ENSIMMÄINEN LAYER OTTAA GREYSCALE IMAGEN (1 CHANNEL 3 SIJAAN)
-        # RuntimeError: The size of tensor a (5) must match the size of tensor b (14) at non-singleton dimension 1
+        self.resnet = models.resnet34(weights='DEFAULT')
         self.resnet.fc = nn.Sequential(
             nn.Dropout(p=0.2),
             nn.Linear(in_features=self.resnet.fc.in_features, out_features=NUM_CLASSES)
@@ -62,6 +60,24 @@ optimizer = torch.optim.Adagrad(model.parameters(), lr=LR)
 # https://towardsdatascience.com/multilabel-classification-with-pytorch-in-5-minutes-a4fa8993cbc7
 loss_function = nn.BCELoss()
 
+#HELPER FUNCTIONS
+
+#turn the sigmoid probabilities to a torch tensor of zeros and ones. One if greater than the specified threshold
+def output_to_prediction(outputs, threshold=0.5):
+    predictions = []
+    for output in outputs:
+        prediction = [np.float32(1.0) if i>threshold else np.float32(0.0) for i in output]
+        predictions.append(prediction)
+    return torch.tensor(predictions).to(device)
+
+#checks if all of the 14 labels are predicted correctly for one image (return True/False)
+def prediction_fully_correct(prediction, target_label):
+    return torch.equal(prediction, target_label)
+
+#checks how many of the labels are predicted correctly for one image (returns int between 0 and 14)
+def correct_labels_in_prediction(prediction, target_labels):
+    return (prediction == target_labels).sum().item()
+
 # Training
 early_stopper = EarlyStopper(patience=3)
 model.train()
@@ -69,6 +85,7 @@ for epoch in range(N_EPOCHS):
     train_loss = 0
     train_correct = 0
     total = 0
+    total_correct_labels = 0
     for batch_number, data in enumerate(image_loader_train):
         images, target_labels = data['image'].to(device), data['target_labels'].to(device)
 
@@ -81,17 +98,26 @@ for epoch in range(N_EPOCHS):
         train_loss += loss.item()
 
         total += target_labels.size(0)
-        _, predicted = torch.max(outputs, 1)
-        train_correct += (predicted == target_labels).sum().item()
+        predictions = output_to_prediction(outputs)
+        for i, prediction in enumerate(predictions):
+            if prediction_fully_correct(prediction, target_labels[i]):
+                train_correct += 1
+            total_correct_labels += correct_labels_in_prediction(prediction, target_labels[i])
 
-        print('Training: Epoch %d - Batch %d/%d: Loss: %.4f | Train Acc: %.3f%% (%d/%d)' % 
-              (epoch, batch_number, len(image_loader_train), train_loss / (batch_number + 1), 
-               100. * train_correct / total, train_correct, total))
+        print('Training: Epoch %d - Batch %d/%d: Loss: %.4f | Train fully correct: %.3f%% (%d/%d) - Train labels correct: %.3f%% (%d/%d)' % 
+              (epoch, batch_number + 1, len(image_loader_train), train_loss / (batch_number + 1), 
+               100. * train_correct / total, train_correct, total, 100. * total_correct_labels / (total*14), total_correct_labels, (total*14)))
         
-'''dataiter = iter(image_loader_train)
-print(len(image_loader_train))
-mini_batch = next(dataiter)
-
-print(mini_batch["image"].shape)
-print(mini_batch["target_labels"].shape)
-'''
+    #validation after each epoch
+    validation_loss = 0
+    with torch.no_grad():
+        for batch_number, data in enumerate(image_loader_validation):
+            images, target_labels = data['image'].to(device), data['target_labels'].to(device)
+            
+            outputs = model(images)
+            
+            loss = loss_function(outputs, target_labels)
+            validation_loss += loss.item()
+            
+    if early_stopper.early_stop(validation_loss):
+        break
